1bb29a6f75acc6e6c5a5f2d6ee423b3f1d41d813
Meet008 <meetchothani008@gmail.com>
2024-01-15 14:27:40 -0500

    portfolio update

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import alpinejs from "@astrojs/alpinejs";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), alpinejs()],
+  output: 'static',
+  publicDir: 'dist',
+  site: 'https://utsavgadhiya.github.io'
+});
---
+++ src/pages/content.js
@@ -0,0 +1,111 @@
+// Personal Information --> #hero section
+export const name = "Meet Chothani";
+export const designation = "Frontend Developer";
+export const location = "Toronto, Canada";
+
+// About
+export const about =
+  "I am a Front-end developer who turns your ideas into reality. I have experience developing a variety of web applications that are fast, efficient, and reliable with beautiful Interfaces.";
+// Work Experience --> #work section
+export const work = [
+  {
+    from: "Sept 2020",
+    to: "Oct 2021",
+    role: "React Developer",
+    org: "IIH Global",
+    org_url: "https://www.iihglobal.com/",
+    location: "Gujarat, India",
+  },
+  {
+    from: "Jan 2020",
+    to: "June 2020",
+    role: "Trainee Software Engineer",
+    org: "Pyther Innovations",
+    org_url: "https://www.pyther.com/",
+    location: "Gujarat, India",
+  },
+];
+
+export const skills = [
+  {
+    skill: "JavaScript",
+    icon: "logos:javascript",
+  },
+  {
+    skill: "React.js",
+    icon: "logos:react",
+  },
+  {
+    skill: "Node.js",
+    icon: "logos:nodejs-icon",
+  },
+  {
+    skill: "Java",
+    icon: "logos:java",
+  },
+  {
+    skill: "C#",
+    icon: "vscode-icons:file-type-csharp2",
+  },
+  {
+    skill: "HTML5",
+    icon: "vscode-icons:file-type-html",
+  },
+  {
+    skill: "CSS3",
+    icon: "vscode-icons:file-type-css",
+  },
+  {
+    skill: "Postgres",
+    icon: "logos:postgresql",
+  },
+  {
+    skill: "MongoDB",
+    icon: "vscode-icons:file-type-mongo",
+  },
+  {
+    skill: "Git",
+    icon: "logos:git-icon",
+  },
+  {
+    skill: "AWS",
+    icon: "logos:aws",
+  },
+];
+
+// Projects --> #project section
+// export const projects = [
+//   {
+//     title: "BookHub",
+//     description: "An online books webapp created with React, Node, MongoDB.",
+//     url: "https://github.com/utsavgadhiya/BookHub",
+//     tech: "JavaScript(Node.js), React, MongoDB",
+//   },
+//   {
+//     title: "ToDoNow",
+//     description:
+//       "Easy-to-use to-do app for effective task management and organization.",
+//     url: "https://github.com/utsavgadhiya/ToDoNow",
+//     tech: "React, Tailwind CSS",
+//   },
+// ];
+
+// Education --> #education section
+export const education = [
+  {
+    from: "2023",
+    to: "2024",
+    institute: "Loyalist College",
+    course: "Post Graduation in Computer Software and Database Development",
+    url: "https://loyalistcollege.com/",
+    location: "Toronto, Canada",
+  },
+  {
+    from: "2016",
+    to: "2020",
+    institute: "Ganpat University",
+    course: "Bachelor of Technology in Computer Science",
+    url: "https://www.ganpatuniversity.ac.in/",
+    location: "Mehsana, India",
+  },
+];
