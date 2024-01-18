51c6d99a81f29b074a96537d7960b17fdcad228b
Meet008 <meetchothani008@gmail.com>
2024-01-17 21:58:55 -0500

    port update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), alpinejs()],
-  output: 'static',
-  publicDir: 'dist',
-  site: 'https://utsavgadhiya.github.io'
+  output: "static",
+  publicDir: "dist",
+  site: "https://meet008.github.io/MeetPORT/",
 });
